f6f1ee3392b33d035a36442c868bfb5351ab773d
Rebeca Galdino <rebecaolg@gmail.com>
2016-07-25 16:54:54 -0300

    Updates

--- app/src/main/java/com/example/rebeca/intentapplicationv3/CalculadoraActivity.java
+++ app/src/main/java/com/example/rebeca/intentapplicationv3/CalculadoraActivity.java
@@ -18,7 +18,7 @@ public class CalculadoraActivity extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_calculator);
+            setContentView(R.layout.activity_calculadora);
 
             GridLayout gl = new GridLayout(this);
 
